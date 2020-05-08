cc8d1366abe956d6b303a14e49a48d44440f7c1e
Stykki <panoari@gmail.com>
2020-05-08 17:06:48 +0000

    updated 10 kindds of humans, still borken

--- python/diff_3_8/10kindsofpeople.py
+++ python/diff_3_8/10kindsofpeople.py
@@ -3,8 +3,10 @@
 class runner:
 
     def __init__(self, start, end, max_x, max_y, run_map):
-        self.binr = 0
-        self.pos_x = int(start[0]) -1
+        self.binr = 0 # set the first test that we will do (starting at binary)
+        
+        # making our positions zero based
+        self.pos_x = int(start[0]) -1   
         self.pos_y = int(start[1]) -1
 
         self.end_x = int(end[0]) -1
@@ -13,10 +15,13 @@ class runner:
         self.max_x = int(max_x) -1
         self.max_y = int(max_y) -1
 
+        # getting our map and creating a previous visited nodes list
         self.rum_map = run_map
         self.prev_nodes = []
 
     def base_run(self):
+        '''Runs our map, starting with binary (if the starting pos is incorrect
+        it will return right away and we can move to the next test)'''
         binary = self.run(self.pos_x, self.pos_y)
         self.binr = 1
         self.prev_nodes = []
@@ -30,12 +35,6 @@ class runner:
             
 
     def run(self, where_x, where_y):
-        #item = self.rum_map[where_x][where_y]
-        #self.rum_map[where_x][where_y] = "x"
-        #print("new step")
-        #for line in self.rum_map:
-        #    print(line)
-        #self.rum_map[where_x][where_y] = item
         
         if self.binr != int(self.rum_map[where_x][where_y]):
             return False
@@ -43,16 +42,16 @@ class runner:
             return True
         self.prev_nodes.append((where_x, where_y))
         passed = False
-        if (where_x + 1 < self.max_x and (where_x+1, where_y) not in self.prev_nodes): # CREATE [X][Y] AND/OR [Y][X]
+        if (where_x + 1 <= self.max_x and (where_x+1, where_y) not in self.prev_nodes): # CREATE [X][Y] AND/OR [Y][X]
             passed = passed or self.run(where_x+1, where_y)
 
-        if (where_y + 1  < self.max_y and (where_x, where_y+1) not in self.prev_nodes):
+        if (where_y + 1  <= self.max_y and (where_x, where_y+1) not in self.prev_nodes):
             passed = passed or self.run(where_x, where_y+1)
 
-        if (where_y - 1  > 0 and (where_x, where_y-1) not in self.prev_nodes):
+        if (where_y - 1  >= 0 and (where_x, where_y-1) not in self.prev_nodes):
             passed = passed or self.run(where_x, where_y-1)
 
-        if (where_x - 1  > 0 and ((where_x-1, where_y)) not in self.prev_nodes):
+        if (where_x - 1  >= 0 and ((where_x-1, where_y)) not in self.prev_nodes):
             passed = passed or self.run(where_x-1, where_y )
 
         return passed
